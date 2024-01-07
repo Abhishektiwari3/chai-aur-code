2c07e930eedfa01c4dbd5b9103a9b6ba3b825f13
Abhishektiwari3 <146344149+Abhishektiwari3@users.noreply.github.com>
2024-01-08 00:19:51 +0530

    redux

---
+++ reduxtoolkittodo/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
+
+function App() {
+
+
+  return (
+    <>
+      <h1>Hello World Redux</h1>
+      <AddTodo />
+      <Todos />
+    </>
+  )
+}
+
+export default App
